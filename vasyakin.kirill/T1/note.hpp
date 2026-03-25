2a09d2eec1e85e52d0f5701daa144e19638c1e6c
potomnazovy <skeleton39876@mail.ru>
2026-03-25 20:01:46 +0300

    add note.hpp file

---
+++ vasyakin.kirill/T1/note.hpp
@@ -0,0 +1,28 @@
+#ifndef NOTE_HPP
+#define NOTE_HPP
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace vasyakin
+{
+  class Note
+  {
+  public:
+    explicit Note(const std::string& name);
+    void noteLine(const std::string& txt);
+    void noteShow() const;
+    void noteLink(const std::weak_ptr< Note >& link);
+    void noteHalt(const std::shared_ptr< Note >& target);
+    void noteMind() const;
+    size_t noteExpired() const;
+    void noteRefresh();
+    const std::string& getName() const;
+  private:
+    std::string name_;
+    std::vector< std::string > lines_;
+    std::vector< std::weak_ptr< Note > > links_;
+  };
+}
+
+#endif
