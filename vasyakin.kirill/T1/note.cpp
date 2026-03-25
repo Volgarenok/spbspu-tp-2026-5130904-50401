a39c372ef8925f9608c928bf44d3189715efae8d
potomnazovy <skeleton39876@mail.ru>
2026-03-25 20:06:14 +0300

    add link and halt methods for note

--- vasyakin.kirill/T1/note.cpp
+++ vasyakin.kirill/T1/note.cpp
@@ -1,9 +1,6 @@
 #include "note.hpp"
 #include <iostream>
 
-#include "note.hpp"
-#include <iostream>
-
 namespace vasyakin
 {
   Note::Note(const std::string& name):
@@ -22,7 +19,47 @@ namespace vasyakin
       std::cout << lines_[i] << '\n';
     }
   }
-  
+
+  void Note::noteLink(const std::weak_ptr< Note >& link)
+  {
+    std::shared_ptr< Note > shr_ptr1 = link.lock();
+    for (auto it = links_.begin(); it != links_.end(); ++it)
+    {
+      if (auto shr_ptr2 = it->lock())
+      {
+        if (shr_ptr1 && shr_ptr2 && shr_ptr1 == shr_ptr2)
+        {
+          return;
+        }
+      }
+    }
+    links_.push_back(link);
+  }
+
+  void Note::noteHalt(const std::shared_ptr< Note >& target)
+  {
+    auto it = links_.begin();
+    while (it != links_.end())
+    {
+      if (auto shr_ptr = it->lock())
+      {
+        if (shr_ptr == target)
+        {
+          it = links_.erase(it);
+          return;
+        }
+        else
+        {
+          ++it;
+        }
+      }
+      else
+      {
+        ++it;
+      }
+    }
+  }
+
   const std::string& Note::getName() const
   {
     return name_;
