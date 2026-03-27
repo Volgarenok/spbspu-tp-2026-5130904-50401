21aacca8f63c5b0e83bd4a3e906b664774a5e432
kstudilova <kstudilova@gmail.com>
2026-03-27 23:39:27 +0300

    add line command with std::quoted

--- studilova.ksenia/T1/commands.cpp
+++ studilova.ksenia/T1/commands.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <iomanip>
 
 #include "commands.hpp"
 
@@ -16,4 +17,24 @@ void studilova::note(std::istream& in, std::ostream& out, Context& ctx)
   n->name = name;
 
   ctx.notes[name] = n;
+}
+
+void studilova::line(std::istream& in, std::ostream&, Context& ctx)
+{
+  std::string name;
+  std::string text;
+
+  in >> name >> std::quoted(text);
+
+  if(!in)
+  {
+    throw std::logic_error("Invalid");
+  }
+
+  auto it = ctx.notes.find(name);
+  if (it == ctx.notes.end())
+  {
+    throw std::logic_error("Invalid");
+  }
+  it->second->lines.push_back(text);
 }
