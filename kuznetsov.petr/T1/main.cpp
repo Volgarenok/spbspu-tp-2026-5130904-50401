f6116952712c37e3bd76d62719231c447440d78d
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-28 18:40:10 +0300

    Add function for create note and update record
    In record.hpp user constructor was created to create note with name and
    without refs and lines

--- kuznetsov.petr/T1/main.cpp
+++ kuznetsov.petr/T1/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "record.hpp"
+
+namespace  kuz = kuznetsov;
+using notepad_t = std::unordered_map< std::string, std::shared_ptr< kuz::Record > >;
+
+void addNote(std::istream& in, notepad_t& db)
+{
+  std::string name;
+  in >> name;
+  if (db.count(name) != 0) {
+    throw std::logic_error("This note already exists");
+  }
+  db[name] = std::make_shared< kuz::Record >(name);
+}
 
 int main()
-{}
+{
+  notepad_t db;
+  std::string cmd;
+  while(std::cin >> cmd) {
+    if (cmd == "add") {
+      addNote(std::cin, db);
+    }
+
+  }
+
+  
+  
+}
 
--- kuznetsov.petr/T1/record.hpp
+++ kuznetsov.petr/T1/record.hpp
@@ -1,12 +1,21 @@
 #ifndef RECORD_HPP
 #define RECORD_HPP
+#include <string>
+#include <vector>
+#include <memory>
 namespace kuznetsov {
-
   struct Record {
-    std::string name;
-    std::vector< std::string > lines;
-    std::vector< std::weak_ptr > refs;
-  }
+    Record(std::string name):
+      name_(name),
+      lines_{},
+      refs_{}
+    {}
+
+    std::string name_;
+    std::vector< std::string > lines_;
+    std::vector< std::weak_ptr< Record > > refs_;
+    
+  };
 }
 #endif
 
