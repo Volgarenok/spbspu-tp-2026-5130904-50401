8479b40e7452221a2d6f92e77cb99bddb0561615
RapidMindd <YaroslavT007@yandex.ru>
2026-03-30 20:40:06 +0300

    show and drop note

--- tarasenko.yaroslav/T1/main.cpp
+++ tarasenko.yaroslav/T1/main.cpp
@@ -10,12 +10,10 @@ struct Note
 {
   std::vector< std::string > data;
   std::vector< std::weak_ptr< Note > > refs;
-  size_t expired_refs;
+  size_t expired_refs = 0;
 };
 
 using unordered_map_of_notes = std::unordered_map< std::string, std::shared_ptr< Note > >;
-using vector_of_strings = std::vector< std::string >;
-using vector_of_weak_ptrs = std::vector< std::weak_ptr< Note > >;
 
 void hello(std::istream&, std::ostream& out, unordered_map_of_notes&)
 {
@@ -30,20 +28,46 @@ void makeNote(std::istream& in, std::ostream&, unordered_map_of_notes& notes)
   {
     throw std::logic_error("Note with this name already exists");
   }
-  notes.emplace(name, std::make_shared< Note >(vector_of_strings{}, vector_of_weak_ptrs{}, 0));
+  notes.emplace(name, std::make_shared< Note >());
 }
 
 void writeLine(std::istream& in, std::ostream&, unordered_map_of_notes& notes)
 {
   std::string name;
   in >> name;
-  if (notes.count(name) > 0)
+  if (notes.count(name) == 0)
   {
-    throw std::logic_error ("No file with such name");
+    throw std::logic_error ("No note with this name");
   }
   std::string line;
   in >> std::quoted(line);
   notes[name]->data.push_back(line);
+}
+
+void showNote(std::istream& in, std::ostream& out, unordered_map_of_notes& notes)
+{
+  std::string name;
+  in >> name;
+  if (notes.count(name) == 0)
+  {
+    throw std::logic_error ("No note with this name");
+  }
+  auto note = notes[name];
+  for (size_t i = 0; i < note->data.size(); ++i)
+  {
+    out << note->data[i] << "\n";
+  }
+}
+
+void dropNote(std::istream& in, std::ostream&, unordered_map_of_notes& notes)
+{
+  std::string name;
+  in >> name;
+  if (notes.count(name) == 0)
+  {
+    throw std::logic_error ("No note with this name");
+  }
+  notes.erase(name);
 }
 
 int main()
@@ -52,8 +76,11 @@ int main()
 
   using cmd_t = void(*)(std::istream&, std::ostream&, unordered_map_of_notes&);
   std::unordered_map< std::string, cmd_t > cmds;
-  cmds["hi!"] = hello;
+  cmds["hi"] = hello;
   cmds["note"] = makeNote;
+  cmds["line"] = writeLine;
+  cmds["show"] = showNote;
+  cmds["drop"] = dropNote;
 
   std::string cmd;
   while (std::cin >> cmd)
