c5143808808cdfc84c7b702fba29cc5983a9b467
RapidMindd <YaroslavT007@yandex.ru>
2026-03-30 10:44:14 +0300

    example copied

--- tarasenko.yaroslav/T1/main.cpp
+++ tarasenko.yaroslav/T1/main.cpp
@@ -1,2 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <unordered_map>
+#include <limits>
+
+void hello()
+{
+  std::cout << "Hello world!\n";
+}
+
+
 int main()
-{}
+{
+  using cmd_t = void(*)();
+  std::unordered_map< std::string, cmd_t > cmds;
+  cmds["hi!"] = hello;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)();
+    }
+    catch (const std::out_of_range&)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toignore, '\n');
+    }
+    catch (const std::logic_error& e)
+    {
+      std::cout << "INVALID COMMAND: " << e.what() << ">\n";
+    }
+  }
+}
