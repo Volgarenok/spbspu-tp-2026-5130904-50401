d4b0fe588b3f8fd017afb73d26fcebdfcb37cdf0
RapidMindd <YaroslavT007@yandex.ru>
2026-03-30 13:27:13 +0300

    makeNote and writeLine

--- tarasenko.yaroslav/T1/main.cpp
+++ tarasenko.yaroslav/T1/main.cpp
@@ -2,25 +2,65 @@
 #include <iomanip>
 #include <unordered_map>
 #include <limits>
+#include <string>
+#include <vector>
+#include <memory>
 
-void hello()
+struct Note
 {
-  std::cout << "Hello world!\n";
+  std::vector< std::string > data;
+  std::vector< std::weak_ptr< Note > > refs;
+  size_t expired_refs;
+};
+
+using unordered_map_of_notes = std::unordered_map< std::string, std::shared_ptr< Note > >;
+using vector_of_strings = std::vector< std::string >;
+using vector_of_weak_ptrs = std::vector< std::weak_ptr< Note > >;
+
+void hello(std::istream&, std::ostream& out, unordered_map_of_notes&)
+{
+  out  << "Hello, World!\n";
 }
 
+void makeNote(std::istream& in, std::ostream&, unordered_map_of_notes& notes)
+{
+  std::string name;
+  in >> name;
+  if (notes.count(name) > 0)
+  {
+    throw std::logic_error("Note with this name already exists");
+  }
+  notes.emplace(name, std::make_shared< Note >(vector_of_strings{}, vector_of_weak_ptrs{}, 0));
+}
+
+void writeLine(std::istream& in, std::ostream&, unordered_map_of_notes& notes)
+{
+  std::string name;
+  in >> name;
+  if (notes.count(name) > 0)
+  {
+    throw std::logic_error ("No file with such name");
+  }
+  std::string line;
+  in >> std::quoted(line);
+  notes[name]->data.push_back(line);
+}
 
 int main()
 {
-  using cmd_t = void(*)();
+  std::unordered_map< std::string, std::shared_ptr< Note > > notes;
+
+  using cmd_t = void(*)(std::istream&, std::ostream&, unordered_map_of_notes&);
   std::unordered_map< std::string, cmd_t > cmds;
   cmds["hi!"] = hello;
+  cmds["note"] = makeNote;
 
   std::string cmd;
   while (std::cin >> cmd)
   {
     try
     {
-      cmds.at(cmd)();
+      cmds.at(cmd)(std::cin, std::cout, notes);
     }
     catch (const std::out_of_range&)
     {
@@ -30,7 +70,7 @@ int main()
     }
     catch (const std::logic_error& e)
     {
-      std::cout << "INVALID COMMAND: " << e.what() << ">\n";
+      std::cout << "<INVALID COMMAND: " << e.what() << ">\n";
     }
   }
 }
