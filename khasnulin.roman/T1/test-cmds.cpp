978c8df79f2b2ce6c9ad292ef9faeb902ee3588a
Spider-has <romanh998@gmail.com>
2026-04-01 22:43:49 +0000

    add test case suite module files

---
+++ khasnulin.roman/T1/test-cmds.cpp
@@ -0,0 +1,7 @@
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_cmds)
+
+BOOST_AUTO_TEST_SUITE_END()
