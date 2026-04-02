907fcb0921b6715721fc12f9432792c529498c1a
aaaa24 <chernov.arseniy@yandex.ru>
2026-04-02 19:44:32 +0300

    add base loop

--- chernov.arseniy/T1/main.cpp
+++ chernov.arseniy/T1/main.cpp
@@ -1,2 +1,16 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
 int main()
-{}
+{
+  std::istream & input = std::cin;
+  std::string cmd;
+  while (input >> cmd) {
+
+  }
+  if (!input.eof()) {
+    std::cerr << "bad input\n";
+    return 1;
+  }
+}
