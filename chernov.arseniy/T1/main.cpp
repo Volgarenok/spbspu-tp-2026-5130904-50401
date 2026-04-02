067ff101ad414495060a445f49ef33dd596c8e5c
aaaa24 <chernov.arseniy@yandex.ru>
2026-04-02 20:18:34 +0300

    create Note

--- chernov.arseniy/T1/main.cpp
+++ chernov.arseniy/T1/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <iomanip>
+#include <memory>
 #include <string>
 #include <unordered_map>
+#include <vector>
+
+namespace chernov {
+  class Note {
+  public:
+    std::string getName() const noexcept;
+    std::string getText() const noexcept;
+  private:
+    std::string name_;
+    std::string text_;
+    std::vector< std::weak_ptr< Note > > links_;
+  };
+}
 
 int main()
 {
   std::istream & input = std::cin;
   std::string cmd;
   while (input >> cmd) {
-
   }
   if (!input.eof()) {
     std::cerr << "bad input\n";
     return 1;
   }
+}
+
+std::string chernov::Note::getName() const noexcept
+{
+  return name_;
+}
+
+std::string chernov::Note::getText() const noexcept
+{
+  return text_;
 }
