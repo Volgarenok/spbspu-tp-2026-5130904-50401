37d45e35cec7ed49e552bac9cec28bb618440c78
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-04-03 23:31:32 +0300

    Implement showLines method

--- burukov.arkadiy/T1/note.cpp
+++ burukov.arkadiy/T1/note.cpp
@@ -13,6 +13,14 @@ namespace burukov
     lines_.push_back(text);
   }
 
+  void Note::showLines() const
+  {
+    for (size_t i = 0; i < lines_.size(); ++i)
+    {
+      std::cout << lines_[i] << '\n';
+    }
+  }
+
   const std::string& Note::getName() const
   {
     return name_;
