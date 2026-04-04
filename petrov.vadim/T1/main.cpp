06bca9c2b6285d629d677a3b0a7fe08b9ce8f75a
Petrov Vadim <vadikdydy@mail.ru>
2026-04-04 18:32:47 +0300

    trash main

--- petrov.vadim/T1/main.cpp
+++ petrov.vadim/T1/main.cpp
@@ -1,2 +1,40 @@
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <limits>
+
 int main()
-{}
+{
+  std::unordered_map< std::string, void(*)(std::vector< std::string >&) > data;
+  data["note"] == ;
+  data["line"] == ;
+  data["show"] == ;
+  data["drop"] == ;
+  data["link"] == ;
+  data["halt"] == ;
+  data["mind"] == ;
+  data["expired"] == ;
+  data["refresh"] == ;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      /* code */
+    }
+    catch(...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toignore, '\n');
+    }
+
+    if (!std::cin.eof())
+    {
+      std::cerr << "BAD INPUT!\n";
+      return 1;
+    }
+  }
+}
