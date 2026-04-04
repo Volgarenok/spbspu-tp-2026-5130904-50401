6412e8e5bf870795d2d6465f3793d6bbd4352d19
StKondrat <kndrt13@icloud.com>
2026-04-04 19:39:30 +0300

    add note.hpp

---
+++ kondrat.stepan/T1/note.hpp
@@ -0,0 +1,19 @@
+#ifndef NOTE_HPP
+#define NOTE_HPP
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+namespace kondrat
+{
+  struct Note
+  {
+    std::string name_;
+    std::vector< std::string > lines_;
+    std::vector< std::weak_ptr< Note > > links_;
+  };
+}
+
+#endif
