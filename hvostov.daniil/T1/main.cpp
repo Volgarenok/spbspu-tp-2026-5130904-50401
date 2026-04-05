98ec654ae5f56f42f9d845882190f5bd0de96beb
PodySIN <daniil2007xboctob@gmail.com>
2026-04-05 22:56:38 +0300

    Add showNote and fix bugs to compile

--- hvostov.daniil/T1/main.cpp
+++ hvostov.daniil/T1/main.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <limits>
 #include <unordered_map>
+#include <memory>
 
 struct Note {
-  std::string name_;
-  std::vector< std::string > lines_;
-  std::vector< std::weak_ptr< Note > > other_notes_;
+  std::string name;
+  std::vector< std::string > lines;
+  std::vector< std::weak_ptr< Note > > other_notes;
+
+  Note(const std::string& n):
+    name(n)
+  {}
 };
 
-void addNote(std::istream&, std::ostream&, std::unordered_map< Note >& notes)
+using NoteBook = std::unordered_map< std::string, std::shared_ptr< Note > >;
+
+void addNote(std::istream& in, std::ostream&, NoteBook& notes)
 {
   std::string name;
   in >> name;
   if (notes.find(name) != notes.end()) {
     throw std::logic_error("Note with this name already exist!");
   }
-  notes[name] = std::make_shared< Note >{name};
+  notes[name] = std::make_shared< Note >(name);
 }
 
-void addLine(std::istream&, std::ostream&, std::unordered_map< Note >& notes)
+void addLine(std::istream& in, std::ostream&, NoteBook& notes)
 {
   std::string name, line;
   in >> name >> std::quoted(line);
@@ -27,38 +38,49 @@ void addLine(std::istream&, std::ostream&, std::unordered_map< Note >& notes)
   notes[name]->lines.push_back(line);
 }
 
-void showNote(std::istream&, std::ostream&, std::unordered_map< Note >& notes)
+void showNote(std::istream& in, std::ostream& out, NoteBook& notes)
 {
-
+  std::string name;
+  in >> name;
+  if (notes.find(name) == notes.end()) {
+    throw std::logic_error("Note with this name doesnt exist!");
+  }
+  auto note = notes[name];
+  for (size_t i = 0; i < note->lines.size(); i++) {
+    out << note->lines[i] << "\n";
+  }
+  if (note->lines.empty()) {
+    out << "\n";
+  }
 }
 
-void dropNote(std::istream&, std::ostream&, std::unordered_map< Note >& notes)
+void dropNote(std::istream&, std::ostream&, NoteBook& notes)
 {
 
 }
 
 int main()
 {
-  std::unordered_map< std::string, Note > notes;
-  using cmd_t = void (*)(std::istream&, std::ostream&, std::unordered_map< Note >&);
+  NoteBook notes;
+  using cmd_t = void (*)(std::istream&, std::ostream&, NoteBook&);
   std::unordered_map< std::string, cmd_t > cmds;
-  cmds["note"] = note;
-  cmds["line"] = line;
-  cmds["show"] = show;
-  cmds["drop"] = drop;
+  cmds["note"] = addNote;
+  cmds["line"] = addLine;
+  cmds["show"] = showNote;
+  cmds["drop"] = dropNote;
   std::string cmd;
   while (std::cin >> cmd) {
     try {
       cmds.at(cmd)(std::cin, std::cout, notes);
     } catch (const std::out_of_range&) {
       std::cout << "<INVALID COMMAND>\n";
-      auto toignore = std::numeric_limits< std::streansize >::max();
+      auto toignore = std::numeric_limits< std::streamsize >::max();
       std::cin.ignore(toignore, '\n');
     } catch (const std::logic_error& e) {
       std::cout << "<INVALID COMMAND: " << e.what() << ">\n";
     }
   }
-  if (!cin.eof()) {
+  if (!std::cin.eof()) {
     std::cerr << "Bad input\n";
     return 1;
   }
