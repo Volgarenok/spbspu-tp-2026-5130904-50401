93eca0d81db1809ab2dc21505c93c66d162414a9
kirusha-13 <kirillukashevich2007@gmail.com>
2026-04-12 21:41:32 +0300

    created notebook.cpp/hpp, implemented comands note/line, declared all commands and struct Note// wrote base  main

--- lukashevich.kirill/T1/main.cpp
+++ lukashevich.kirill/T1/main.cpp
@@ -1,4 +1,43 @@
+#include "notebook.hpp"
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <limits>
 int main()
-{}
+{
+  using namespace lukashevich;
+  Notebook nb;
+  using cmd_t = void (Notebook::*)(std::istream &, std::ostream &, const std::string &);
+  std::unordered_map< std::string, cmd_t > cmds;
+  cmds["note"] = &Notebook::note;
+  cmds["line"] = &Notebook::line;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      std::string name;
+      std::cin >> name;
+      (nb.*cmds.at(cmd))(std::cin, std::cout, name);
+    }
+    catch (const std::out_of_range &)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toignore, '\n');
+    }
+    catch (const std::logic_error &e)
+    {
+      std::cout << "<INVALID COMMAND: " << e.what() << ">\n";
+    };
+  }
+
+  if (!std::cin.eof())
+  {
+    std::cerr << "Bad input\n";
+    return 1;
+  }
+  return 0;
+}
---
+++ lukashevich.kirill/T1/notebook.cpp
@@ -0,0 +1,34 @@
+#include "notebook.hpp"
+
+lukashevich::Note::Note(std::string name):
+  name_(name)
+{}
+
+void lukashevich::Notebook::note(std::istream&, std::ostream&, const std::string& name)
+{
+  for (size_t i = 0; i < notes_.size(); ++i)
+  {
+    if (notes_[i].name_ == name)
+    {
+      throw std::logic_error("this name already exists");
+    }
+  }
+
+  Note newNote = Note(name);
+  notes_.push_back(newNote);
+}
+
+void lukashevich::Notebook::line(std::istream& in, std::ostream&, const std::string& name)
+{
+  for (size_t i = 0; i < notes_.size(); ++i)
+  {
+    if (notes_[i].name_ == name)
+    {
+      std::string newLine;
+      in >> std::quoted(newLine);
+      notes_[i].text_.push_back(newLine);
+      return;
+    }
+  }
+  throw std::logic_error("there is no note with this name");
+}
